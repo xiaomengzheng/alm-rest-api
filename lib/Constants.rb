e04d9d6aa9eadfa233251e303fcf829c0597fe58
xiaomengzheng <xiaomengzheng@gmail.com>
2012-10-22 12:38:22 -0700

    constants

---
+++ lib/Constants.rb
@@ -0,0 +1,12 @@
+# These constants are used throughout the code to set the server to work with.
+# To execute this code, change these settings to fit
+# those of your server.
+class Constants
+  HOST = "localhost"
+  PORT = "8080"
+  USERNAME = "sa"
+  PASSWORD = ""
+  DOMAIN = "DEFAULT"
+  PROJECT = "testProject"
+  VERSIONED = true
+end
