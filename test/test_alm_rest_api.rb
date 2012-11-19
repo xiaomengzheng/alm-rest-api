4dc03f860d7c2531e88d32db3b0e34343c9701da
xiaomengzheng <xiaomengzheng@gmail.com>
2012-11-18 23:43:28 -0800

    test

--- test/test_alm_rest_api.rb
+++ test/test_alm_rest_api.rb
@@ -16,50 +16,54 @@ class TestALMRestAPI < Test::Unit::TestCase
     # Returns nil if authenticated. If not authenticated, returns
     # a URL indicating where to login.
     # We are not logged in, so call returns a URL
-    ##authenticationPoint = ALM.isAuthenticated()
-    ##assert_not_nil(authenticationPoint, "response from isAuthenticated means we're authenticated. that can't be.")
+    authenticationPoint = ALM.isAuthenticated()
+    assert_not_nil(authenticationPoint, "response from isAuthenticated means we're authenticated. that can't be.")
     
     # now we login to previously returned URL.
-    ##loginResponse = ALM.login(authenticationPoint, ALM::Constants::USERNAME, ALM::Constants::PASSWORD)
-    ##assert(loginResponse, "failed to login.")
-    ##assert((ALM::RestConnector.instance.getCookieString.include? "LWSSO_COOKIE_KEY"), "login did not cause creation of Light Weight Single Sign On(LWSSO) cookie.")
+    loginResponse = ALM.login(authenticationPoint, ALM::Constants::USERNAME, ALM::Constants::PASSWORD)
+    assert(loginResponse, "failed to login.")
+    assert((ALM::RestConnector.instance.getCookieString.include? "LWSSO_COOKIE_KEY"), "login did not cause creation of Light Weight Single Sign On(LWSSO) cookie.")
     
     # proof that we are indeed logged in
-    ##assert_nil(ALM.isAuthenticated(), "isAuthenticated returned not authenticated after login.")
+    assert_nil(ALM.isAuthenticated(), "isAuthenticated returned not authenticated after login.")
     
-    ## and now we logout
-    ##ALM.logout()
+    # and now we logout
+    ALM.logout()
     
     # And now we can see that we are indeed logged out
     # because isAuthenticated once again returns a url, and not null.
-    ##assert_not_nil(ALM.isAuthenticated(), "isAuthenticated returned authenticated after logout.")    
+    assert_not_nil(ALM.isAuthenticated(), "isAuthenticated returned authenticated after logout.")    
   end
   
   def test_GetDefectFields
-    #loginResponse = ALM.isLoggedIn(ALM::Constants::USERNAME, ALM::Constants::PASSWORD)
-    #assert(loginResponse, "failed to login.")
+    loginResponse = ALM.isLoggedIn(ALM::Constants::USERNAME, ALM::Constants::PASSWORD)
+    assert(loginResponse, "failed to login.")
 
-    #defectFields = ALM.getDefectFields(true)
-    #valueLists = ALM.getValueLists(defectFields)
-    #defectFields.fields.each do |field|
-      #puts "Name = #{field.name}"
-      #puts "Label = #{field.label}"
-      #puts "Size = #{field.size}"
-      #puts "Type = #{field.type}"
-      #puts "Required = #{field.required}"
-      #if (field.list_id && valueLists)
-        #items = ValueLists.getValuesById(field.list_id, valueLists)
-        #items.each do |item|
-          #puts "Value = #{item.value}"
-        #end
-      #end
-      #puts "--------------------------"
-    #end
+    defectFields = ALM.getDefectFields(true)
+    valueLists = ALM.getValueLists(defectFields)
+    if defectFields
+      defectFields.fields.each do |field|
+        puts "Name = #{field.name}"
+        puts "Label = #{field.label}"
+        puts "Size = #{field.size}"
+        puts "Type = #{field.type}"
+        puts "Required = #{field.required}"
+        if (field.list_id && valueLists)
+          items = ValueLists.getValuesById(field.list_id, valueLists)
+          items.each do |item|
+            puts "Value = #{item.value}"
+          end
+        end
+        puts "--------------------------"
+      end
     
-    #puts defectFields.to_xml
-    #puts valueLists.to_xml   
+      puts defectFields.to_xml
+      if valueLists
+        puts valueLists.to_xml 
+      end
+    end  
     
-    #ALM.logout()
+    ALM.logout()
   end
   
   def test_CreateDeleteDefect
@@ -70,22 +74,28 @@ class TestALMRestAPI < Test::Unit::TestCase
     
     defectFields = ALM.getDefectFields(true)
     valueLists = ALM.getValueLists(defectFields)
-    defectFields.fields.each do |field|
-      defectField = Field.new
-      defectField.name = field.name
-      if (field.list_id && valueLists)
-        items = ValueLists.getValuesById(field.list_id, valueLists)
-        defectField.value = items.first.value
-      else
-        defectField.value = "0"
-      end
-      defect.fields.fields<<defectField
-    end    
-
-    puts defect.to_xml
-    defectUrl = ALM.createDefect(defect)
+    if defectFields
+      defectFields.fields.each do |field|
+        defectField = Field.new
+        defectField.name = field.name
+        if (field.list_id && valueLists)
+          items = ValueLists.getValuesById(field.list_id, valueLists)
+          defectField.value = items.first.value
+        else
+          defectField.value = "0"
+        end
+        defect.fields.fields<<defectField
+      end    
+      
+      puts defect.to_xml
+      
+      defectUrl = ALM.createDefect(defect)
+      puts defectUrl
     
-    ALM.deleteDefect(defectUrl)
+      if defectUrl
+        deleteResponse = ALM.deleteDefect(defectUrl)
+      end
+    end
     
     ALM.logout()
   end
